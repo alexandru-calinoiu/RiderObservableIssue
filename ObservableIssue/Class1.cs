4ace44067a1a5c1a53de12c0c07358d6aca0c013
Calin <calinoiu.alexandru@agilefreaks.com>
2017-09-06 18:32:06 +0200

    Add all

---
+++ ObservableIssue/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace ObservableIssue
+{
+    public class Class1
+    {
+        public void Main()
+        {
+            Observable.FromAsync(_ => new Task<int>(() => 42));
+            Login().SelectMany(_ => Observable.Empty<ResponseClass>());
+        }
+
+        private IObservable<ResponseClass> Login()
+        {
+            return new List<ResponseClass> { new ResponseClass(), new ResponseClass() }.ToObservable();
+        }
+
+        class ResponseClass
+        {
+            public string Body { get; set; }
+        }
+    }
+}
